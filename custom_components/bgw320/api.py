78a8e04739f5b628ac65012e0f6ff9d57542340b
tmcb82 <tmcb82@gmail.com>
2026-05-21 15:58:23 -0400

    Update api.py

--- custom_components/bgw320/api.py
+++ custom_components/bgw320/api.py
@@ -14,7 +14,7 @@ class BGW320Client:
         data = {
             "software_version": self._extract_value(sysinfo_html, "Software Version"),
             "uptime_seconds": self._extract_value(sysinfo_html, "Time Since Last Reboot"),
-            "fiber_link_status": self._extract_value(stats_html, "Link Status"),
+            "pon_link_status": self._extract_value(stats_html, "PON Link Status"),
             "broadband_connection": self._extract_value(stats_html, "Broadband Connection"),
             "bytes_transmitted": self._extract_value(stats_html, "Transmit Bytes"),
             "bytes_received": self._extract_value(stats_html, "Receive Bytes")
